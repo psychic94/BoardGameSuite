3c24e6d356e989f5057051d4754700b9295c8494
psychic94 <psychic94@gmail.com>
2014-05-22 21:37:33 -0400

    Added temporary values to support a new parameter

--- src/psy/boardgames/chess/ChessBoard.java
+++ src/psy/boardgames/chess/ChessBoard.java
@@ -1,54 +1,62 @@
 package psy.boardgames.chess;
 
-import org.bukkit.Location;
+import com.sk89q.worldedit.LocalWorld;
+import com.sk89q.worldedit.Vector;
 
-public class ChessBoard {
-	Location a1, h8;
+import psy.boardgames.chess.set.Bishop;
+import psy.boardgames.chess.set.ChessPiece;
+import psy.boardgames.chess.set.King;
+import psy.boardgames.chess.set.Knight;
+import psy.boardgames.chess.set.Pawn;
+import psy.boardgames.chess.set.Queen;
+import psy.boardgames.chess.set.Rook;
+import psy.boardgames.core.GameBoard;
+
+public class ChessBoard extends GameBoard{
 	ChessPiece[][] pieces;
-	boolean whitesTurn;
-	
-	public ChessBoard(){
-		
+
+	public ChessBoard(String name, LocalWorld world, Vector pos1, Vector pos2){
+		super(name, world, pos1, pos2);
 	}
-	
+
 	public void setUp(){
+        
 		//Initialize
 		pieces = new ChessPiece[8][8];
 		
 		//White's back rank
-		pieces[0][0] = new Rook(this, false, 0, 0);
-		pieces[0][1] = new Knight(this, false, 0, 1);
-		pieces[0][2] = new Bishop(this, false, 0, 2);
-		pieces[0][3] = new Queen(this, false, 0, 3);
-		pieces[0][4] = new King(this, false, 0, 4);
-		pieces[0][5] = new Bishop(this, false, 0, 5);
-		pieces[0][6] = new Knight(this, false, 0, 6);
-		pieces[0][7] = new Rook(this, false, 0, 7);
+		pieces[0][0] = new Rook(this, false, null, 0, 0);
+		pieces[0][1] = new Knight(this, false, null, 0, 1);
+		pieces[0][2] = new Bishop(this, false, null, 0, 2);
+		pieces[0][3] = new Queen(this, false, null, 0, 3);
+		pieces[0][4] = new King(this, false, null, 0, 4);
+		pieces[0][5] = new Bishop(this, false, null, 0, 5);
+		pieces[0][6] = new Knight(this, false, null, 0, 6);
+		pieces[0][7] = new Rook(this, false, null, 0, 7);
 		
 		//White's pawns
 		for(byte i=0; i<8; i++)
-			pieces[1][i] = new Pawn(this, false, 1, i);
+			pieces[1][i] = new Pawn(this, false, null, 1, i);
 		
 		//Black's back rank
-		pieces[7][0] = new Rook(this, true, 7, 0);
-		pieces[7][1] = new Knight(this, true, 7, 1);
-		pieces[7][2] = new Bishop(this, true, 7, 2);
-		pieces[7][3] = new Queen(this, true, 7, 3);
-		pieces[7][4] = new King(this, true, 7, 4);
-		pieces[7][5] = new Bishop(this, true, 7, 5);
-		pieces[7][6] = new Knight(this, true, 7, 6);
-		pieces[7][7] = new Rook(this, true, 7, 7);
+		pieces[7][0] = new Rook(this, true, null, 7, 0);
+		pieces[7][1] = new Knight(this, true, null, 7, 1);
+		pieces[7][2] = new Bishop(this, true, null, 7, 2);
+		pieces[7][3] = new Queen(this, true, null, 7, 3);
+		pieces[7][4] = new King(this, true, null, 7, 4);
+		pieces[7][5] = new Bishop(this, true, null, 7, 5);
+		pieces[7][6] = new Knight(this, true, null, 7, 6);
+		pieces[7][7] = new Rook(this, true, null, 7, 7);
 		
 		//Blacks's pawns
 		for(byte i=0; i<8; i++)
-			pieces[6][i] = new Pawn(this, true, 6, i);
+			pieces[6][i] = new Pawn(this, true, null, 6, i);
 		
 		//Empty squares
 		for(int i=2; i<6; i++)
 			for(int j=0; j<8; j++)
 				pieces[i][j] = null;
 		
-		whitesTurn = true;
 	}
 	
 	public void setSquare(int rank, int file, ChessPiece piece){
